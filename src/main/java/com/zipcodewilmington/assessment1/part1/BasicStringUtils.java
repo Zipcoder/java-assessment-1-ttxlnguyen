f98de4b7865d17eac47a3f07608403218289a3ff
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-08 10:26:04 -0400

    finished, camelcase, reverse, removefirstandlast, invertcasing

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,8 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+//        return null;
     }
 
     /**
@@ -17,7 +18,11 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String string = str;
+        String reverse = new StringBuffer(string).reverse().toString();
+        new StringBuilder(str).reverse().toString();
+        return reverse;
+//        return null;
     }
 
     /**
@@ -34,7 +39,8 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1, str.length() - 1);
+//        return null;
     }
 
     /**
@@ -42,6 +48,17 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char[] characters = str.toCharArray();
+        for (int i = 0; i < characters.length; i++)
+        {
+            char c = characters[i];
+            if (Character.isUpperCase(c)) {
+                characters[i] = Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                characters[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(characters);
+//        return null;
     }
 }
